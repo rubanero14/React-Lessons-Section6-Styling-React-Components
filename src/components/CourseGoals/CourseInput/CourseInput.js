20397c359afb93877c9f64986bced2eb2ce72f7d
Rajaruban Rajindram <ahyat888@gmail.com>
2022-11-11 22:30:18 +0800

    Dynamic Styles with CSS Modules

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,35 +1,36 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
+import styles from './CourseInput.module.css';
+// import styled from 'styled-components';
 import Button from '../../UI/Button/Button';
 
-const FormControl = styled.div`
-  margin: 0.5rem 0;
+// const FormControl = styled.div`
+//   margin: 0.5rem 0;
 
-  & label {
-    font-weight: bold;
-    display: block;
-    margin-bottom: 0.5rem;
-    // Dynamically changing styling using props passed from component to make leaner css code base
-    color: ${props => props.invalid ? 'red' : "#333"}
-  }
+//   & label {
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//     // Dynamically changing styling using props passed from component to make leaner css code base
+//     color: ${props => props.invalid ? 'red' : "#333"}
+//   }
 
-  & input {
-    display: block;
-    width: 100%;
-    // Dynamically changing styling using props passed from component to make leaner css code base
-    border: 1px solid ${props => props.invalid ? 'red' : '#ccc'};
-    background: ${props => props.invalid ? 'rgb(243, 136, 136)' : "transparent"};
-    font: inherit;
-    line-height: 1.5rem;
-    padding: 0 0.25rem;
-  }
+//   & input {
+//     display: block;
+//     width: 100%;
+//     // Dynamically changing styling using props passed from component to make leaner css code base
+//     border: 1px solid ${props => props.invalid ? 'red' : '#ccc'};
+//     background: ${props => props.invalid ? 'rgb(243, 136, 136)' : "transparent"};
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//   }
 
-  & input:focus {
-    outline: none;
-    background: #fad0ec;
-    border-color: #8b005d;
-  }
-`;
+//   & input:focus {
+//     outline: none;
+//     background: #fad0ec;
+//     border-color: #8b005d;
+//   }
+// `;
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -50,13 +51,24 @@ const CourseInput = props => {
     props.onAddGoal(enteredValue);
   };
 
-  return (
+  /*
+    // Styled Component codes
+     <form onSubmit={formSubmitHandler}>
+       // Passing props with key as can be used to dynamically change styling in styled div component above 
+        <FormControl invalid={!isValid}>
+          <label>Course Goal</label>
+          <input type="text" onChange={goalInputChangeHandler} />
+        </FormControl>
+        <Button type="submit">Add Goal</Button>
+      </form>
+  */
+
+  return (  
     <form onSubmit={formSubmitHandler}>
-      {/* Passing props with key as can be used to dynamically change styling in styled div component above */}
-      <FormControl invalid={!isValid}>
+      <div className={`${styles["form-control"]} ${!isValid && styles.invalid}`}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-      </FormControl>
+      </div>
       <Button type="submit">Add Goal</Button>
     </form>
   );
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -29,7 +29,6 @@ import styles from './Button.module.css';
 // `;
 
 const Button = props => {
-  console.log(styles);
   return(
     <button type={props.type} className={styles.button} onClick={props.onClick}>
       {props.children}
